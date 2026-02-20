c3d9d58459bc28f9e8d92937bb0c7c8b25935050
sufihasan <hasan6nh@gmail.com>
2026-02-20 20:59:16 +0600

    make loader.tsx for show loading

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+    return (
+        <div className="flex justify-center items-center py-20">
+            <div className="animate-spin rounded-full h-12 w-12 border-4 border-gray-300 border-t-black"></div>
+        </div>
+    );
+}
